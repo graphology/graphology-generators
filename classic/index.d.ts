d32fbe6ef0d20cbe1d17409c12e9e99bde9745cf
Yomguithereal <guillaumeplique@gmail.com>
2020-11-09 19:59:28 +0100

    Type declarations for classic graphs

---
+++ classic/index.d.ts
@@ -0,0 +1,4 @@
+export {default as complete} from './complete';
+export {default as empty} from './complete';
+export {default as ladder} from './complete';
+export {default as path} from './complete';
